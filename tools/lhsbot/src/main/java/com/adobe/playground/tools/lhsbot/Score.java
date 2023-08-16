4132524b2dc67cf1487efa989aee22937c8ab56d
Remo Liechti <141611642+RemoLiechti@users.noreply.github.com>
2023-08-16 11:53:48 +0200

    make it multithreading (#5)
    * make it MT
    
    * add screenshot path back
    
    * put proper date
    
    * added main branch urls, too

--- tools/lhsbot/src/main/java/com/adobe/playground/tools/lhsbot/Score.java
+++ tools/lhsbot/src/main/java/com/adobe/playground/tools/lhsbot/Score.java
@@ -4,19 +4,14 @@ import java.util.Date;
 
 public class Score {
 
-	public String url, performance, accessibility, bestpractices, seo;
-	public Date captureDate;
-	private String screenshot;
+	private static int nextRun = 0;
 
-	public Score(String url, String performance, String accessibility, String bestpractices, String seo,
-			String screenshot) {
-		this.performance = performance;
-		this.accessibility = accessibility;
-		this.bestpractices = bestpractices;
-		this.seo = seo;
-		this.url = url;
-		this.screenshot = screenshot;
-		this.captureDate = new Date();
+	Integer nr;
+	String url, performance, accessibility, bestpractices, seo,screenshot;
+	Date captureDate;
+
+	public Score() {
+		nr = ++nextRun;
 	}
 
 	/*
