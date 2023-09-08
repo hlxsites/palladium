15e6988ce8442207eaf3307094bef2791aa0c039
Paolo Mottadelli <paolo.moz@gmail.com>
2023-09-08 14:45:48 +0200

    hero (#36)
    * hero
    
    * fix: eslint
    
    * test: tbt without radial gradient
    
    * test: put radial gradient back again and see TBT

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,6 +11,8 @@ import {
   waitForLCP,
   loadBlocks,
   loadCSS,
+  createOptimizedPicture,
+  getMetadata,
 } from './lib-franklin.js';
 
 const LCP_BLOCKS = []; // add your LCP blocks to the list
@@ -20,14 +22,14 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
  * @param {Element} main The container element
  */
 function buildHeroBlock(main) {
-  const h1 = main.querySelector('h1');
-  const picture = main.querySelector('picture');
-  // eslint-disable-next-line no-bitwise
-  if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
-  }
+  const p = document.createElement('p');
+  p.textContent = getMetadata('hero-subtitle');
+  const h1 = document.createElement('h1');
+  h1.textContent = getMetadata('h1');
+  const picture = createOptimizedPicture(getMetadata('hero-picture'), 'Hero banner', true);
+  const section = document.createElement('div');
+  section.append(buildBlock('hero', { elems: [picture, p, h1] }));
+  main.prepend(section);
 }
 
 /**
