f7258a555fc4bdcf603a9cbbc2a9c0f7b9576935
Paolo Mottadelli <paolo.moz@gmail.com>
2023-09-13 16:22:14 +0200

    google tag instrumentation

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,30 @@ import { sampleRUM } from './lib-franklin.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+/**
+ * Google Tag
+* */
+function loadGAScript(url, callback) {
+  const head = document.querySelector('head');
+  let script = head.querySelector(`script[src="${url}"]`);
+  if (!script) {
+    script = document.createElement('script');
+    script.src = url;
+    script.async = true;
+    head.append(script);
+    script.onload = callback;
+    return script;
+  }
+  return script;
+}
+
+async function loadGA() {
+  const gaId = 'G-Q7JCVTBTKM';
+  loadGAScript(`https://www.googletagmanager.com/gtag/js?id=${gaId}`, () => {
+  // eslint-disable-next-line
+    window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', gaId);
+  });
+}
+
+await loadGA();
