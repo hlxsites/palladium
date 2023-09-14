2729b4b3d56c862ccecd0962291f45198e07e391
Paolo Mottadelli <paolo.moz@gmail.com>
2023-09-14 18:18:44 +0200

    newsletter form

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -19,14 +19,23 @@ export default async function decorate(block) {
     const footer = document.createElement('div');
     footer.innerHTML = html;
 
+    decorateIcons(footer);
+    decorateButtons(footer);
+    block.append(footer);
+
     const formTitle = footer.querySelector('.center > div > div > h4');
     const input = document.createElement('input');
     input.setAttribute('type', 'email');
     input.setAttribute('placeholder', 'Email');
     formTitle.after(input);
-
-    decorateIcons(footer);
-    decorateButtons(footer);
-    block.append(footer);
+    const button = footer.querySelector('.center a.button');
+    button.classList.add('disabled');
+    const href = button.href;
+    
+    input.addEventListener('keyup', (event) => {
+      if (event.target.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) button.classList.remove('disabled');
+      else button.classList.add('disabled');
+      button.href = `${href}${event.target.value}`;
+    });
   }
 }
