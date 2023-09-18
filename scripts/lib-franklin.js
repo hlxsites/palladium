b47895b5e19c16a6ca726ad2dc9a4d8bacc87eec
Paolo Mottadelli <paolo.moz@gmail.com>
2023-09-18 22:33:55 +0200

    a11y links (#63)
    * a11y links
    
    * accessible icon links
    
    * a11y heading order
    
    * footer links tap space
    
    * fix: linting
    
    * fix: linting
    
    * fix: linting
    
    * fix: linting

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -227,6 +227,7 @@ export async function decorateIcons(element) {
     } else {
       parent.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg"><use href="#icons-sprite-${iconName}"/></svg>`;
     }
+    if (span.parentElement.tagName === 'A' && !span.parentElement.title) span.parentElement.setAttribute('aria-label', iconName);
   });
 }
 
