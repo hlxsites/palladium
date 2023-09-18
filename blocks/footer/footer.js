ab1eec66a76327baf98d0cf5d292da46e47b4e97
Paolo Mottadelli <paolo.moz@gmail.com>
2023-09-18 20:48:32 +0200

    prod footr content

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -9,7 +9,7 @@ export default async function decorate(block) {
   block.textContent = '';
 
   // fetch footer content
-  const footerPath = cfg.footer || '/drafts/paolom/footer';
+  const footerPath = cfg.footer || '/footer';
   const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
