217d649a9df6238200dbe5a15d6d2ed86426960b
Paolo Mottadelli <paolo.moz@gmail.com>
2023-09-28 17:42:05 +0200

    fix /hlx/footer typo

--- hlx/blocks/footer/footer.js
+++ hlx/blocks/footer/footer.js
@@ -9,7 +9,7 @@ export default async function decorate(block) {
   block.textContent = '';
 
   // fetch footer content
-  const footerPath = cfg.footer || '/hxl/footer';
+  const footerPath = cfg.footer || '/hlx/footer';
   const resp = await fetch(`${footerPath}.plain.html`, window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {});
 
   if (resp.ok) {
